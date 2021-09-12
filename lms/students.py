2b1834c9f4c027b5b04bd5fe4cc2163d618217c8
katyaklem <75757152+katyaklem@users.noreply.github.com>
2021-09-13 01:23:52 +0300

    Sep11

---
+++ lms/students.py
@@ -0,0 +1,43 @@
+student_fields = ['first_name', 'last_name', 'gender', 'age', 'email', 'address']
+
+STUDENTS = []
+TEST_STUDENTS = [['Marry', 'Pop', 'F', '32', 'marry@pop', 'Od'],['Garry', 'Bob', 'M', '22', 'garry@bob', 'Bi']]
+
+def add_student():
+	student = {}
+	for field in student_fields:
+		student[field] = input('Enter {}'.format(field))
+	STUDENTS.append(student)
+
+def print_student(student):
+	for field in student:
+		print(field, '\t', student[field])
+
+def load_students():
+	for test_student in TEST_STUDENTS:
+		student = dict(zip(student_fields, test_student))
+		STUDENTS.append(student)
+
+def calculator_avg_age():
+	total_age = 0
+	for student in STUDENTS:
+		total_age += int(student['age'])
+	avg_age = total_age // len(STUDENTS)
+	print(avg_age)
+
+
+while True:
+	action = input('Your action:')
+	if action == 'add':
+		add_student()
+	elif action == 'load':
+		load_students()
+	elif action == 'print':
+		for student in STUDENTS:
+			print_student(student)
+	elif action == 'age':
+		calculator_avg_age()
+	else:
+		break
+
+
